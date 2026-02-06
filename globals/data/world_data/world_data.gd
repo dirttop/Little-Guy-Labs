19df48fd8b40291cd999b04b3f0396eacbc1a363
Gleb Syomichev <103398854+dirttop@users.noreply.github.com>
2026-02-06 14:32:55 -0500

    feat: world management
    Started world management features.

--- globals/data/world_data/world_data.gd
+++ globals/data/world_data/world_data.gd
@@ -1,3 +1,9 @@
 @tool
 extends Resource
 class_name WorldData
+
+@export_group("current")
+@export var active_scene: PackedScene
+@export var spawn_pos: Vector3
+@export var save_scene: PackedScene
+@export var save_pos: Vector3
