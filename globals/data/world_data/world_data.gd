9a3f680e9f4bff2a6d3bff431390244dcffce2df
Gleb Syomichev <103398854+dirttop@users.noreply.github.com>
2026-02-08 17:07:17 -0500

    fix: world data
    Fixing some merge mistakes.

--- globals/data/world_data/world_data.gd
+++ globals/data/world_data/world_data.gd
@@ -4,6 +4,6 @@ class_name WorldData
 
 @export_group("current")
 @export var active_scene: PackedScene
-@export var spawn_pos: Vector3
+@export var spawn_point: String
 @export var save_scene: PackedScene
 @export var save_pos: Vector3
