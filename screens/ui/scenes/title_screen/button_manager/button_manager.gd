957121f74d6849199982e5abcb56a4a774d8c7db
Gleb Syomichev <103398854+dirttop@users.noreply.github.com>
2026-03-04 12:12:33 -0500

    fix: title screen

--- screens/ui/scenes/title_screen/button_manager/button_manager.gd
+++ screens/ui/scenes/title_screen/button_manager/button_manager.gd
@@ -1,9 +1,17 @@
 extends Node
 @onready var animation: AnimationPlayer = $"../AnimationPlayer"
+@onready var main_menu: Control = $Margin/Main
+@onready var saves_menu: Control = $Margin/Saves
+
+func _ready() -> void:
+	saves_menu.visible = false
+	main_menu.visible = true
 
 func _on_start_pressed() -> void:
 	animation.play("hide_main")
 	await animation.animation_finished
+	main_menu.visible = false
+	saves_menu.visible = true
 	animation.play("show_saves")
 
 func _on_options_pressed() -> void:
@@ -18,4 +26,6 @@ func _on_quit_pressed() -> void:
 func _on_back_pressed() -> void:
 	animation.play("hide_saves")
 	await animation.animation_finished
+	saves_menu.visible = false
+	main_menu.visible = true
 	animation.play("show_main")
